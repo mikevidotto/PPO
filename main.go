95aad3ed3d1c23ec06def24b6d5f5fefc2bfbaa6
i4ni <mikevidotto@live.com>
2025-05-08 18:42:20 -0400

    feat: calculate returns, advantages. save transition data.

--- main.go
+++ main.go
@@ -11,42 +11,6 @@ import (
 	"reflect"
 )
 
-// training loop:
-// VALUES NETWORK
-// 1. estimate value of current state (x)
-//
-//
-// POLICY NETWORK
-// 1. feed current position (x) into policy network
-// 2. 3 hidden layer neurons:
-//      ->tanh((x * weight) + bias)
-//      ->tanh((x * weight) + bias)
-//      ->tanh((x * weight) + bias)
-// 3. pass 3 values into 3 output neurons to get actions
-//      ->((x1 * weight) + (x2 * weight) + (x3 * weight)) + bias
-// 4. get action probability distribution vector from softmax function
-//      ->softmax(output1, output2, output3) will create a vector
-//      ->example result: [0.1, 0.3, 0.6]
-// 5. Choose an action (randomly for exploration)
-// 6. Apply the action to the environment
-// 7. Compute the reward
-//      ->+1 if on target
-//      ->+0 if not
-//      LATER: ->-1 for each step
-// 8. Log the transition data
-//
-// VALUE NETWORK
-// 1. pass updated state values network
-// 2. estimate the "value" of the given position based on reward
-// 3. Log the transition data for advantage calculation
-//
-// repeat until target is reached or max steps is reached
-//
-// Take the episode data and:
-// 1. Compute the return
-// 2. Compute the advantage
-// 3. Store it batch training
-
 const (
 	maxSteps    = 50
 	maxPosition = 100
@@ -61,9 +25,10 @@ type TransitionData struct {
 type StepData struct {
 	StateValue float64 //result of V(x)
 
-	StatePosition     int     //current position: x
+	State             int     //current position: x
 	Action            int     // left: -1, stay: 0 or right: 1
 	ActionProbability float64 //result of P(x)
+	NextState         int     // current position + action: x + action (-1, 0, 1)
 	Reward            int     //-1 for each step, +50 for reaching target
 	Done              bool    //true if x = target
 
@@ -119,170 +84,208 @@ func main() {
 	//  ->load environment
 	//  ->load the network
 
-	Episode := TransitionData{}
+	EpisodeData := TransitionData{}
 	step := StepData{}
 
 	env := Environment{}
 	env.Reset()
 
+	ppo := PPO{}
+	ppo, err := ppo.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	//START EPISODE with 20 STEPS MAX
 	for range maxSteps {
 		if !step.Done {
 
-		//env, err := env.Load()
-		//if err != nil {
-		//	log.Fatal(err)
-		//}
-		step.Initialize()
-		step.StatePosition = env.PlayerPosition
-		step.DistanceToTarget = env.TargetPosition - step.StatePosition
+			step.Reward = 0 //automatically -1 reward for each step taken
 
-		ppo := PPO{}
-		ppo, err := ppo.Load()
-		//run values network for current state
-		estimatedvalue := ppo.Values.OutputLayer(ppo.Values.HiddenLayer(env.PlayerPosition))
-		step.StateValue = estimatedvalue
+			step.State = env.PlayerPosition
+			step.DistanceToTarget = target - env.PlayerPosition
 
-		//run policy network
+			//run values network for current state
+			estimatedvalue := ppo.Values.OutputLayer(ppo.Values.HiddenLayer(env.PlayerPosition))
+			step.StateValue = estimatedvalue
 
-		outputs := ppo.Policy.OutputLayer(ppo.Policy.HiddenLayer(env.PlayerPosition))
+			//run policy network
 
-		//outputs := p.OutputLayer(p.HiddenLayer(env.PlayerPosition))
-		probabilityDistribution := Softmax(outputs)
+			outputs := ppo.Policy.OutputLayer(ppo.Policy.HiddenLayer(env.PlayerPosition))
 
-		action, probability := StochasticSample(probabilityDistribution)
-		if action == 2 {
-			fmt.Println("error getting action...")
-		}
+			//outputs := p.OutputLayer(p.HiddenLayer(env.PlayerPosition))
+			probabilityDistribution := Softmax(outputs)
 
-		step.Action = action
-		step.ActionProbability = probability
+			action, probability := StochasticSample(probabilityDistribution)
+			if action == 2 {
+				fmt.Println("error getting action...")
+			}
 
-		if env.PlayerPosition == target {
-			step.Done = true
-		}
+			step.Action = action
+			step.ActionProbability = probability
 
-		//apply action from probabilityDistribution
-		if env.PlayerPosition+action > 100 {
-			env.PlayerPosition = 100
-		} else if env.PlayerPosition+action < 0 {
-			env.PlayerPosition = 0
-		} else {
-			env.PlayerPosition += action
-		}
+			if env.PlayerPosition == target {
+				step.Done = true
+			}
 
-		//compute reward. should be -1 by default and then check if we are on the target and reward 50 points if we are.
-		step.Reward += -1
-		if env.PlayerPosition == target {
-			step.Reward += 50
-		}
+			//apply action from probabilityDistribution
+			if env.PlayerPosition+action > 100 {
+				env.PlayerPosition = 100
+			} else if env.PlayerPosition+action < 0 {
+				env.PlayerPosition = 0
+			} else {
+				env.PlayerPosition += action
+			}
 
-		//compute advantage and save data or something
+			step.NextState = env.PlayerPosition
 
-		storedPPO, err := ppo.Load()
-		if err != nil {
-			log.Fatal(err)
-		}
-		if !reflect.DeepEqual(ppo, storedPPO) {
-			err = ppo.Save()
+			//compute reward. should be -1 if we are further from the target and 0 if we are closer to then check if we are on the target and reward 50 points if we are.
+			margin := (env.TargetPosition - env.PlayerPosition)
+			if margin < 0 {
+				if step.DistanceToTarget < 0 {
+					if (margin * -1) == (step.DistanceToTarget * -1) {
+                        step.Reward += 0
+					} else if (margin * -1) > (step.DistanceToTarget * -1) {
+						step.Reward += -1
+					} else {
+						step.Reward += 2
+					}
+
+				} else {
+					if (margin * -1) == step.DistanceToTarget {
+                        step.Reward += 0
+                    } else if (margin * -1) > step.DistanceToTarget {
+						step.Reward += -1
+					} else {
+						step.Reward += 2
+					}
+				}
+			} else {
+				if step.DistanceToTarget < 0 {
+					if margin == (step.DistanceToTarget * -1) {
+                        step.Reward += 0
+                    } else if margin > (step.DistanceToTarget * -1) {
+						step.Reward += -1
+					} else {
+						step.Reward += 2
+					}
+
+				} else {
+					if margin == step.DistanceToTarget {
+                        step.Reward += 0
+                    } else if margin > step.DistanceToTarget {
+						step.Reward += -1
+					} else {
+						step.Reward += 2
+					}
+
+				}
+			}
+			if env.PlayerPosition == target {
+				step.Reward += 50
+			}
+
+			//compute advantage and save data or something
+
+			storedPPO, err := ppo.Load()
 			if err != nil {
 				log.Fatal(err)
 			}
+			if !reflect.DeepEqual(ppo, storedPPO) {
+				err = ppo.Save()
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			EpisodeData.Steps = append(EpisodeData.Steps, step)
+			step.StepIndex++
 		}
-
-		//storedEnv, err := env.Load()
-		//if err != nil {
-		//	log.Fatal(err)
-		//}
-		//if !reflect.DeepEqual(env, storedEnv) {
-		//	fmt.Println("Saving because changes have been made to the Environment.")
-		//	err = env.Save()
-		//	if err != nil {
-		//		log.Fatal(err)
-		//	}
-		//} else {
-		//	fmt.Println("No changes made to the Environment, therefore we won't update init file.")
-		//}
-
-		Episode.Steps = append(Episode.Steps, step)
-		step.StepIndex++
-        }
 	}
 
-    //log/store the episode data to calculate the advantage and return
+	//function: log/store the episode data using TransitionData{}
+	EpisodeData.SaveData()
 
+	//function:calculate the advantage and return
+	returnvalues := Returns(EpisodeData.Steps, ppo)
+	advantagevalue := Advantage(EpisodeData.Steps, ppo, returnvalues)
 
-	for _, step := range Episode.Steps {
-		fmt.Printf("step: %2d, x: %2d, %4d units to target, V(%d): %4f action: %2d, action prob: %4f, reward: %2d, done:  %4t\n", step.StepIndex, step.StatePosition, step.DistanceToTarget, step.StatePosition, step.StateValue, step.Action, step.ActionProbability, step.Reward, step.Done)
-		//fmt.Printf("Step %d \nx position: %d: %d units to target(50) \nstate value: %f \nAction: %d \nActionProbability: %f\nReward: %d\nDone: %t\n-----------------------------------------------\n", step.StepIndex, step.StatePosition, step.DistanceToTarget, step.StateValue, step.Action, step.ActionProbability, step.Reward, step.Done)
+	fmt.Println("returns:", returnvalues)
+	fmt.Println("advantage:", advantagevalue)
+	//we have the ppo data inside the latest.txt file.
+	//->function: populate a temp ppo and return it with the adjusted values
+	//->assign current ppo to this function before saving so that the old ppo is backed up before it is adjusted
+
+	for _, step := range EpisodeData.Steps {
+		fmt.Printf("step: %2d, x: %2d, %4d units to target, V(%d): %4f action: %2d, action prob: %4f, nextposition: %2d, reward: %2d, done:  %4t\n", step.StepIndex, step.State, step.DistanceToTarget, step.State, step.StateValue, step.Action, step.ActionProbability, step.NextState, step.Reward, step.Done)
+
+		//fmt.Printf("Step %d \nx position: %d: %d units to target(50) \nstate value: %f \nAction: %d \nActionProbability: %f\nReward: %d\nDone: %t\n-----------------------------------------------\n", step.StepIndex, step.State, step.DistanceToTarget, step.StateValue, step.Action, step.ActionProbability, step.Reward, step.Done)
 	}
 }
 
-func (data *StepData) Initialize() {
+func Returns(steps []StepData, ppo PPO) (finalreturns []float64) {
+	var returns []float64
+	gamma := 0.99
+	returnvalue := 0.0
+	returnsum := 0.0
+	i := 0
+	for t := len(steps) - 1; t >= 0; t-- {
+		if t == len(steps)-1 {
+			returnvalue = float64(steps[t].Reward)
+			returnsum = returnvalue
+			returns = append(returns, returnsum)
+		} else {
+			returnvalue = float64(steps[t].Reward) + (gamma * returnsum)
+			returnsum = returnvalue
+			returns = append(returns, returnsum)
+		}
+		i++
+	}
 
-	//data.StatePosition = 0
-	//data.DistanceToTarget = 0
-	//data.StateValue = 0.0
+	for j := len(returns) - 1; j >= 0; j-- {
+		finalreturns = append(finalreturns, returns[j])
+	}
 
-	//data.Action = 0
-	//data.ActionProbability = 0.0
-
-	//data.Reward = 0
-
-	//data.StepIndex = 0
-	//data.Done = false
+	return finalreturns
 }
 
-//func (env *Environment) Load() (Environment, error) {
-//	var savedEnvironment Environment
-//
-//	if !ff.FileExists("./init/env/latest.txt") {
-//		//reset to default values
-//		savedEnvironment.Reset()
-//		return savedEnvironment, nil
-//	}
-//	file, err := os.Open("./init/env/latest.txt")
-//	if err != nil {
-//		return Environment{}, err
-//	}
-//	defer file.Close()
-//
-//	decoder := json.NewDecoder(file)
-//	err = decoder.Decode(&savedEnvironment)
-//	if err != nil {
-//		return Environment{}, err
-//	}
-//
-//	return savedEnvironment, nil
-//}
-//
-//func (env *Environment) Save() error {
-//	bytes, err := json.Marshal(env)
-//	if err != nil {
-//		return err
-//	}
-//	//create backup of current network data
-//	if ff.FileExists("./init/env/latest.txt") {
-//		data, err := os.ReadFile("./init/env/latest.txt")
-//		if err != nil {
-//			return err
-//		}
-//		file, err := os.CreateTemp("./history/env/", "*.txt")
-//		if err != nil {
-//			return err
-//		}
-//		_, err = file.Write(data)
-//		if err != nil {
-//			return err
-//		}
-//	}
-//	err = os.WriteFile("./init/env/latest.txt", bytes, 0666)
-//	if err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
+func Advantage(steps []StepData, ppo PPO, returns []float64) (advantages []float64) {
+	var advantage float64
+	for i, step := range steps {
+		advantage = returns[i] - step.StateValue
+		advantages = append(advantages, advantage)
+	}
+
+	return advantages
+}
+
+func (td *TransitionData) SaveData() error {
+	fmt.Println("HELOLO???")
+	bytes, err := json.Marshal(td)
+	if err != nil {
+		return err
+	}
+	if ff.FileExists("./transitions/latest_transition.txt") {
+		data, err := os.ReadFile("./transitions/latest_transition.txt")
+		if err != nil {
+			return err
+		}
+		file, err := os.CreateTemp("./history/transitions/", "*.txt")
+		if err != nil {
+			return err
+		}
+		_, err = file.Write(data)
+		if err != nil {
+			return err
+		}
+	}
+	err = os.WriteFile("./transitions/latest_transition.txt", bytes, 0666)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (p *PPO) Save() error {
 	bytes, err := json.Marshal(p)
